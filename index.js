802359555fa8d9ccfbd363c0e83b670802336756
Carlos Amado <exchangeside@gmail.com>
2022-06-24 09:53:57 -0400

    img

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ const keybugfix = {
 // }, 2000);
 ravensSource = '../images/RavensFix.png'
 owlsSource = '../images/owls fix.png'
-platformRockImg = '../images/platformimg1.png'
+platformRockImg = '../images/platformimg1.gif'
 let bottomLeftRock = new PlatformClass(platformRockImg, 4, {
     x: 135,
     y: 490,
