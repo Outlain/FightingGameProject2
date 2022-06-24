7464196f7d0aac08a7dea1e51eec7c05c392c54d
Carlos Amado <exchangeside@gmail.com>
2022-06-24 07:31:52 -0400

    final Img bug fix

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ const keybugfix = {
 // }, 2000);
 ravensSource = '../images/RavensFix.png'
 owlsSource = '../images/owls fix.png'
-platformRockImg = '../images/floating-platform-spritesheet.png'
+platformRockImg = '../images/floatingplatformspritesheet.png'
 let bottomLeftRock = new PlatformClass(platformRockImg, 4, {
     x: 135,
     y: 490,
